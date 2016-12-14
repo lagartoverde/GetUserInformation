1965786008416f09ac07d9650164867e072d9cfa
Oscar Rodriguez Villalobos <lagartoverde97@gmail.com>
2016-12-14 21:49:33 +0100

    added some files to solve the bugs with node start

---
+++ server.js
@@ -0,0 +1,16 @@
+var http=require("http");
+
+var server=http.createServer(function(req,res){
+	var ip=req.connection.remoteAddress;
+	var lang=req.headers["accept-language"];
+	var userAgent=req.headers["user-agent"];
+	var os=userAgent.split("(")[1].split(")")[0];
+	var responseObject={
+		"Ip":ip,
+		"Language":lang,
+		"Operating System":os
+	}
+	res.end(JSON.stringify(responseObject));
+});
+server.listen("8080");
+console.log("Server Running");
